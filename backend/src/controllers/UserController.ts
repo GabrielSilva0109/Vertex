f4ee89660e523ab2a394a0e25e99debf10004636
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-02-19 12:50:36 -0300

    Controller User

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -11,3 +11,27 @@ export const getUsers = (req: Request, res: Response) => {
         return res.status(200).json(data)
     })
 }
+
+export const getUserById = (req: Request, res:Response) => {
+    const q = "SELECT * FROM users WHERE `id`=?;"
+
+    db.query(q, [req.params.id], (erro, data) =>{
+        if(erro) return res.status(500).json({erro: 'Erro ao encontrar Usuario'})
+
+        return res.status(200).json(data[0])
+    })
+}
+
+export const createUser = (req: Request, Res:Response) =>{
+
+}
+
+export const updateUser = (req: Request, Res:Response) =>{
+    
+}
+
+export const deleteUser = (req: Request, Res:Response) =>{
+    
+}
+
+
--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,9 +1,12 @@
 import express from 'express'
-import { getUsers } from '../controllers/UserController'
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from '../controllers/UserController'
 
 const router = express.Router()
 
 router.get('/users', getUsers)
-
+router.get('/users/:id', getUserById)
+router.post('/user', createUser)
+router.put('/user', updateUser)
+router.delete('user', deleteUser)
 
 export default router
