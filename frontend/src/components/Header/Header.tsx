6d97e6615bd8c2c907532142ebfe8407845790b1
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-02-20 10:26:01 -0300

    Header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,27 +4,57 @@ import styled from 'styled-components'
 const HeaderStyle = styled.div`
     background: black;
     display: flex;
+    justify-content: space-between;
+    padding: 0px 20px;
 `
 
 const TitleBank = styled.div`
     color: white;
 `
 
+const NavLinks = styled.div`
+    color: white;
+    font-size: 1.2rem;
+    font-weight: 600;
+    display: flex;
+    justify-content: center;
+`
+const UlLinks = styled.ul`
+    display: flex;
+    align-items: center;
+    gap: 30px;
+    list-style: none;
+`
+
+const BtnLogin = styled.button`
+    background: #b0ff00;
+    padding: 12px 18px;
+    border-radius: 20px;
+    color: black;
+    font-size: 1rem;
+    font-weight: bold;
+    border: none;
+
+    &:hover{
+        background: #d3fd74;
+    }
+`
+
 const Header: React.FC = () =>{
     return (
-        <HeaderStyle className='header'>
-            <TitleBank className='title-header'>
+        <HeaderStyle>
+            <TitleBank>
                 <h1>Vertex Bank</h1>
             </TitleBank>
-            <div className='nav-links'>
-                <ul>
+            <NavLinks>
+                <UlLinks>
                     <li>Inicio</li>
                     <li>Sobre</li>
                     <li>Beneficios</li>
                     <li>Contato</li>
-                </ul>
-                <button className='btn-login'>Login</button>
-            </div>
+                    <li><BtnLogin className='btn-login'>Login</BtnLogin></li>
+                </UlLinks>               
+            </NavLinks>
         </HeaderStyle>
     )
 }
