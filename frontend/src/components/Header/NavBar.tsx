80cf5af669be64fabbaf8a7a9e8451fe5730d409
GabrielSilva0109 <silvagabriel0109@gmail.com>
2024-02-23 12:12:53 -0300

    Update NavBar.tsx

--- frontend/src/components/Header/NavBar.tsx
+++ frontend/src/components/Header/NavBar.tsx
@@ -64,7 +64,59 @@ const BtnSair = styled(Link)`
     overflow: hidden;
     transition-duration: 0.3s;
     box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
-    background-color: #b0ff00
+    background-color: #b0ff00;
+    text-decoration: none;
+    
+    &:hover {
+        width: 125px;
+        border-radius: 40px;
+        transition-duration: 0.3s;
+    }
+
+    &:hover .sign {
+        width: 30%;
+        transition-duration: 0.3s;
+        padding-left: 20px;
+    }
+
+    &:active {
+        transform: translate(2px, 2px);
+    }
+`
+
+const Sign = styled.div`
+    width: 100%;
+    transition-duration: 0.3s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    
+    svg {
+        width: 18px;
+        
+    }
+
+    svg path {
+        fill: black;
+    }
+`
+
+const Text = styled.div`
+    opacity: 0;
+    width: 0px;
+    color: black;
+    font-size: 1.5em;
+    font-weight: 600;
+    transition-duration: 0.3s;
+    text-decoration: none;
+    ${BtnSair}:hover & {
+        opacity: 1;
+        width: 70%;
+        transition-duration: 0.3s;
+        padding-right: 10px;
+        text-decoration: none;
+    }
 `
 
 const NavBar: React.FC = () => {
@@ -86,7 +138,16 @@ const NavBar: React.FC = () => {
             </Links>
 
             <Config>
-                <BtnSair to="/login">Sair</BtnSair>
+                <BtnSair to="/login">
+                    <Sign>
+                        <svg viewBox="0 0 512 512">
+                        <path
+                            d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"
+                        ></path>
+                        </svg>
+                        <Text>&nbsp;&nbsp;&nbsp;&nbsp;Sair</Text>
+                    </Sign>
+                </BtnSair>
 
             </Config>
         </Nav>
