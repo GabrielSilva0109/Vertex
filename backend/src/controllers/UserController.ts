14442018872c15a03d12c9423d32ce11163875ba
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-03-05 08:57:20 -0300

    Update UserController.ts

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -33,16 +33,16 @@ export const getUserById = (req: Request, res:Response) => {
 
 // Cria um Usuario
 export const createUser = (req: Request, res: Response) => {
-    const { name, password, email, birth, cpf, cep } = req.body;
+    const { name, password, email, birth, cpf, cep } = req.body
 
     if (!name || !password || !email) {
-        return res.status(400).json({ error: 'Campos Obrigatórios!' });
+        return res.status(400).json({ error: 'Campos Obrigatórios!' })
     }
 
     // Criptografa a senha antes de armazenar
-    const hashedPassword = bcrypt.hashSync(password, 10);
+    const hashedPassword = bcrypt.hashSync(password, 10)
 
-    const q = "INSERT INTO users (`name`, `password`,`email`,`birth`, `cpf`, `cep`) VALUES (?,?,?,?,?,?);";
+    const q = "INSERT INTO users (`name`, `password`,`email`,`birth`, `cpf`, `cep`) VALUES (?,?,?,?,?,?);"
     db.query(q, [name, hashedPassword, email, birth, cpf, cep], (erro, data) => {
         if (erro) return res.status(500).json({ erro: 'Erro ao Cadastrar Usuário' })
 
@@ -53,10 +53,10 @@ export const createUser = (req: Request, res: Response) => {
         const contaNumber = generateRandomNumber()
 
         // Cria uma wallet para o novo usuário
-        const walletQuery = "INSERT INTO wallets (`user_id`, `conta`, `saldo`) VALUES (?,?,?);";
+        const walletQuery = "INSERT INTO wallets (`user_id`, `conta`, `saldo`) VALUES (?,?,?);"
         db.query(walletQuery, [userId, contaNumber, 0.00], (walletError, walletData) => {
             if (walletError) {
-                return res.status(500).json({ error: 'Erro ao Criar Wallet para o Usuário' });
+                return res.status(500).json({ error: 'Erro ao Criar Wallet para o Usuário' })
             }
 
             return res.status(201).json('Usuário e Wallet Criados!!');
@@ -101,60 +101,45 @@ export const updateUser = (req: Request, res: Response) => {
 
 //Deleta o Usuario
 export const deleteUser = (req: Request, res: Response) => {
-    const userId = req.params.id;
+    const userId = req.params.id
 
-    const q = "DELETE FROM users WHERE `id`=?";
+    const q = "DELETE FROM users WHERE `id`=?"
     db.query(q, [userId], (erro, data) => {
-        if (erro) return res.status(500).json({ erro: 'Erro ao Deletar Usuário' });
-
-        return res.status(200).json('Usuário Deletado!!');
-    });
+        if (erro) return res.status(500).json({ erro: 'Erro ao Deletar Usuário' })
+        return res.status(200).json('Usuário Deletado!!')
+    })
 }
 
 // Realiza o Login
 export const loginUser = async (req: Request, res: Response) => {
     try {
         const { cpf, password } = req.body
-
-        console.log('Tentativa de login com CPF:', cpf)
-
         const q = "SELECT * FROM users WHERE cpf=?"
-
         const result: any[] = await new Promise((resolve, reject) => {
             db.query(q, [cpf], (error, result) => {
                 if (error) {
-                    console.error('Erro ao buscar usuário no banco de dados:', error);
-                    reject(error);
+                    console.error('Erro ao buscar usuário no banco de dados:', error)
+                    reject(error)
                 } else {
-                    console.log('Resultado da consulta ao banco de dados:', result);
-
                     if (result.length > 0) {
-                        const user = result[0];
-
+                        const user = result[0]
                         // Compara a senha fornecida com o hash armazenado no banco de dados
-                        const passwordMatch = bcrypt.compareSync(password, user.password);
-
-                        console.log('Senha correspondente?', passwordMatch);
+                        const passwordMatch = bcrypt.compareSync(password, user.password)
 
                         if (passwordMatch) {
-                            console.log('Login bem-sucedido. Enviando informações do usuário:', user);
                             res.status(200).json(user);
                         } else {
-                            res.status(401).json({ message: 'Credenciais inválidas pela senha' });
+                            res.status(401).json({ message: 'Credenciais inválidas pela senha' })
                         }
                     } else {
-                        res.status(401).json({ message: 'Credenciais inválidas result' });
+                        res.status(401).json({ message: 'Credenciais inválidas result' })
                     }
-
-                    resolve(result);
+                    resolve(result)
                 }
-            });
-        });
+            })
+        })
     } catch (error) {
-        console.error('Erro ao processar a solicitação de login:', error);
-        res.status(500).json({ message: 'Erro interno do servidor' });
+        console.error('Erro ao processar a solicitação de login:', error)
+        res.status(500).json({ message: 'Erro interno do servidor' })
     }
-};
-
-
-
+}
