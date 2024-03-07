9259076785a326403bcb6c1b3b78bc833cc1a83a
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-03-07 11:39:32 -0300

    Update CarteiraContent.tsx

--- frontend/src/components/Carteira/CarteiraContent.tsx
+++ frontend/src/components/Carteira/CarteiraContent.tsx
@@ -141,8 +141,117 @@ const ImgPerfil = styled.div`
   }
 `
 
+const Btns = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap:5px;
+`
+
 const BtnDelete = styled.button`
-  height: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 15px;
+  background-color: rgb(255, 0, 0);
+  cursor: pointer;
+  border: none;
+  transition-duration: 0.3s;
+
+  &:hover {
+    background-color: rgb(255, 95, 95);
+  }
+
+  &:active {
+    transform: scale(0.9);
+  }
+
+  .bin-bottom {
+    width: 15px;
+  }
+
+  .bin-top {
+    width: 17px;
+    transform-origin: right;
+    transition-duration: 0.3s;
+  }
+
+  &:hover .bin-top {
+    transform: rotate(45deg);
+  }
+`
+
+const BtnEdit = styled.button`
+width: 40px;
+height: 40px;
+border-radius: 15px;
+border: none;
+background-color: #2c2c2c;
+display: flex;
+align-items: center;
+justify-content: center;
+box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.123);
+cursor: pointer;
+position: relative;
+overflow: hidden;
+transition: all 0.3s;
+
+&::before {
+  content: "";
+  width: 200%;
+  height: 200%;
+  background-color: rgb(102, 102, 141);
+  position: absolute;
+  z-index: 1;
+  transform: scale(0);
+  transition: all 0.3s;
+  border-radius: 50%;
+  filter: blur(10px);
+}
+
+&:hover::before {
+  transform: scale(1);
+}
+
+&:hover {
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.336);
+}
+
+svg {
+  height: 17px;
+  fill: white;
+  z-index: 3;
+  transition: all 0.2s;
+  transform-origin: bottom;
+}
+
+&:hover svg {
+  transform: rotate(-15deg) translateX(5px);
+}
+
+&::after {
+  content: "";
+  width: 25px;
+  height: 1.5px;
+  position: absolute;
+  bottom: 19px;
+  left: -5px;
+  background-color: white;
+  border-radius: 2px;
+  z-index: 2;
+  transform: scaleX(0);
+  transform-origin: left;
+  transition: transform 0.5s ease-out;
+}
+
+&:hover::after {
+  transform: scaleX(1);
+  left: 0px;
+  transform-origin: right;
+}
 `
 
 interface Transacao {
@@ -185,7 +294,10 @@ const CarteiraContent: React.FC = () =>{
     
       return `${dia}/${mes}/${ano}`
     }
-
+    const formatarSaldo = (saldo: number): string => {
+      return saldo.toFixed(2)
+    }
+    
     const handleCheckboxChange = () => {
       setIsDespesa((prevValue) => !prevValue);
     }
@@ -447,15 +559,56 @@ const CarteiraContent: React.FC = () =>{
               {extrato.map((transacao) => (
                 <ExtratoItem key={transacao.id} categoria={transacao.categoria}>
                   <p>{transacao.titulo}</p>
-                  <p>{transacao.valor}</p>
+                  <p>R${formatarSaldo(transacao.valor)}</p>
                   <p>{transacao.observacao}</p>
-                  <p>{transacao.categoria}</p>
                   <p>{transacao.fonte}</p>
                   <p>{formatarData(transacao.data)}</p>
-                  <div>
-                    <BtnDelete>Editar</BtnDelete>
-                    <BtnDelete onClick={() => deleteTransacao(transacao.id,  transacao.categoria, transacao.valor)}>Excluir</BtnDelete>
-                  </div>
+                  <Btns>
+                    <BtnEdit >
+                      <svg height="1em" viewBox="0 0 512 512">
+                        <path
+                          d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
+                        ></path>
+                      </svg>
+                    </BtnEdit>
+                    <BtnDelete onClick={() => deleteTransacao(transacao.id,  transacao.categoria, transacao.valor)}>
+                    <svg
+                          className="bin-top"
+                          viewBox="0 0 39 7"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <line y1="5" x2="39" y2="5" stroke="white" stroke-width="4"></line>
+                          <line
+                            x1="12"
+                            y1="1.5"
+                            x2="26.0357"
+                            y2="1.5"
+                            stroke="white"
+                            stroke-width="3"
+                          ></line>
+                        </svg>
+                        <svg
+                          className="bin-bottom"
+                          viewBox="0 0 33 39"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <mask id="path-1-inside-1_8_19" fill="white">
+                            <path
+                              d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
+                            ></path>
+                          </mask>
+                          <path
+                            d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
+                            fill="white"
+                            mask="url(#path-1-inside-1_8_19)"
+                          ></path>
+                          <path d="M12 6L12 29" stroke="white" stroke-width="4"></path>
+                          <path d="M21 6V29" stroke="white" stroke-width="4"></path>
+                        </svg>
+                    </BtnDelete>
+                  </Btns>
                 </ExtratoItem>
               ))}
             </Extrato>
