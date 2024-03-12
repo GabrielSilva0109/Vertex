ce672eab5f0ec45096b017bfa9f97ddad2460aa5
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-03-12 09:51:49 -0300

    Update InvestimentoController

--- backend/src/controllers/InvestimentoController.ts
+++ backend/src/controllers/InvestimentoController.ts
@@ -3,71 +3,70 @@ import { Request, Response } from 'express'
 
 // Retorna todos os Investimentos
 export const getInvestimentos = async (req: Request, res: Response) => {
-    const q = "SELECT * FROM ativos;";  
+    const q = "SELECT * FROM investimentos;"  
     db.query(q, (erro, data) => {
-        if (erro) return res.status(500).json({ erro: "Erro ao trazer os Ativos" });
-        return res.status(200).json(data);
-    });
+        if (erro) return res.status(500).json({ erro: "Erro ao trazer os Investimentos" })
+        return res.status(200).json(data)
+    })
 }
 
 // Retorna Investimento por ID
 export const getInvestimentoById = async (req: Request, res: Response) => {
-    const q = "SELECT * FROM ativos WHERE `id`=?;"; 
+    const q = "SELECT * FROM investimentos WHERE `id`=?;"
 
     db.query(q, [req.params.id], (erro, data) => {
-        if (erro) return res.status(500).json({ erro: "Erro ao trazer Ativo por ID" });
-        return res.status(200).json(data[0]);
-    });
+        if (erro) return res.status(500).json({ erro: "Erro ao trazer Ativo por ID" })
+        return res.status(200).json(data[0])
+    })
 }
 
 // Retorna Investimento por ID da WALLET
 export const getInvestimentoByWalletId = async (req: Request, res: Response) => {
-    const q = "SELECT * FROM Ativos WHERE `wallet_id`=?"
+    const q = "SELECT * FROM investimentos WHERE `wallet_id`=?"
 
     db.query(q, [req.params.id], (erro, data) => {
-        if(erro) return res.status(500).json({erro: "Erro ao trazer os Ativos dessa Wallet"})
+        if(erro) return res.status(500).json({erro: "Erro ao trazer os Investimentos dessa Wallet"})
         return res.status(200).json(data)
     })
 }
 
 // Cria o Investimento
 export const createInvestimento = async (req: Request, res: Response) => {
-    const { wallet_id, titulo, valor, observacao, categoria, fonte, data } = req.body; 
+    const { wallet_id, titulo, valor, observacao, categoria, data } = req.body; 
 
     if (!titulo || !valor || !wallet_id) {
         return res.status(400).json({ erro: "Campos Obrigatórios!" });
     }
 
-    const q = "INSERT INTO ativos(`wallet_id`, `titulo`, `valor`, `observacao`, `categoria`, `fonte`, `data`) VALUES (?,?,?,?,?,?,?);";  // Alteração do nome da tabela para 'ativos'
-    db.query(q, [wallet_id, titulo, valor, observacao, categoria, fonte, data], (erro, data) => {
-        if (erro) return res.status(500).json({ erro: "Erro ao Cadastrar o Ativo" });
-        return res.status(201).json("Cadastrado Ativo!");
-    });
+    const q = "INSERT INTO ativos(`wallet_id`, `titulo`, `valor`, `observacao`, `categoria`, `data`) VALUES (?,?,?,?,?,?);";  // Alteração do nome da tabela para 'ativos'
+    db.query(q, [wallet_id, titulo, valor, observacao, categoria, data], (erro, data) => {
+        if (erro) return res.status(500).json({ erro: "Erro ao Cadastrar o Investimento" })
+        return res.status(201).json("Investimento Cadastrado!")
+    })
 }
 
 // Atualiza o Investimento 
 export const updateInvestimento = async (req: Request, res: Response) => {
-    const ativoId = req.params.id;
-    const { titulo, valor, observacao, categoria, fonte, data } = req.body;  
+    const ativoId = req.params.id
+    const { titulo, valor, observacao, categoria, data } = req.body
 
     // Construir a parte SET dinamicamente com base nos campos fornecidos pelo usuário
     const setFields = [];
-    if (titulo !== undefined) setFields.push("titulo=?");
-    if (valor !== undefined) setFields.push("valor=?");
-    if (observacao !== undefined) setFields.push("observacao=?");
-    if (categoria !== undefined) setFields.push("categoria=?");
-    if (fonte !== undefined) setFields.push("fonte=?");
-    if (data !== undefined) setFields.push("data=?");
+    if (titulo !== undefined) setFields.push("titulo=?")
+    if (valor !== undefined) setFields.push("valor=?")
+    if (observacao !== undefined) setFields.push("observacao=?")
+    if (categoria !== undefined) setFields.push("categoria=?")
+    if (data !== undefined) setFields.push("data=?")
 
     if (setFields.length === 0) {
         return res.status(400).json({ erro: "Nenhum campo fornecido para atualização" })
     }
 
-    const q = `UPDATE ativos SET ${setFields.join(", ")} WHERE id=?;`
+    const q = `UPDATE investimentos SET ${setFields.join(", ")} WHERE id=?;`
 
     db.query(q, [...Object.values(req.body).filter(value => value !== undefined), ativoId], (erro, data) => {
-        if (erro) return res.status(500).json({ erro: "Erro ao Atualizar o Ativo" })
-        return res.status(200).json("Ativo Atualizado!")
+        if (erro) return res.status(500).json({ erro: "Erro ao Atualizar o Investimento" })
+        return res.status(200).json("Investimento Atualizado!")
     });
 }
 
@@ -75,10 +74,10 @@ export const updateInvestimento = async (req: Request, res: Response) => {
 export const deleteInvestimento = async (req: Request, res: Response) => {
     const ativoId = req.params.id;
 
-    const q = "DELETE FROM investimentos WHERE id=?;";  // Alteração do nome da tabela para 'ativos'
+    const q = "DELETE FROM investimentos WHERE id=?;"
 
     db.query(q, [ativoId], (erro, data) => {
-        if (erro) return res.status(500).json({ erro: "Erro ao Excluir o Investimento " });
-        return res.status(200).json("Investimento Excluído!");
-    });s
+        if (erro) return res.status(500).json({ erro: "Erro ao Excluir o Investimento " })
+        return res.status(200).json("Investimento Excluído!")
+    })
 }
