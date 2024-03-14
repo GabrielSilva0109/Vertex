798b63727dcff0d154706e4251decdea179d13d0
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-03-14 12:38:48 -0300

    Update Grafico.tsx

--- frontend/src/components/Graficos/Grafico.tsx
+++ frontend/src/components/Graficos/Grafico.tsx
@@ -2,19 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 interface Ativo {
-  data: string | number | Date;
-  titulo: string;
-  valor: number;
+  data: string | number | Date
+  titulo: string
+  valor: number
 }
 
 interface Despesa {
-  data: string | number | Date;
-  titulo: string;
-  valor: number;
+  data: string | number | Date
+  titulo: string
+  valor: number
 }
 
 interface GraficoProps {
-  walletId: number;
+  walletId: number
 }
 
 const CustomTooltip = ({ active, payload, label }: any) => {
@@ -28,31 +28,31 @@ const CustomTooltip = ({ active, payload, label }: any) => {
       </div>
     );
   }
-  return null;
+  return null
 }
 
 
 const Grafico: React.FC<GraficoProps> = ({ walletId }) => {
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<any[]>([])
 
   const fetchData = async () => {
     try {
-      const responseAtivos = await fetch(`http://localhost:3333/ativosWallet/${walletId}`);
-      const responseDespesas = await fetch(`http://localhost:3333/despesasWallet/${walletId}`);
+      const responseAtivos = await fetch(`http://localhost:3333/ativosWallet/${walletId}`)
+      const responseDespesas = await fetch(`http://localhost:3333/despesasWallet/${walletId}`)
       
-      const ativos: Ativo[] = await responseAtivos.json();
-      const despesas: Despesa[] = await responseDespesas.json();
+      const ativos: Ativo[] = await responseAtivos.json()
+      const despesas: Despesa[] = await responseDespesas.json()
 
-      const dataByMonth = groupDataByMonth(ativos, despesas);
-      setData([...dataByMonth]);
+      const dataByMonth = groupDataByMonth(ativos, despesas)
+      setData([...dataByMonth])
     } catch (error) {
-      console.error('Erro ao buscar dados:', error);
+      console.error('Erro ao buscar dados:', error)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchData();
-  }, [walletId]);
+    fetchData()
+  }, [walletId])
 
   const groupDataByMonth = (ativos: Ativo[], despesas: Despesa[]) => {
     const groupedData: any = {}
@@ -89,21 +89,20 @@ const Grafico: React.FC<GraficoProps> = ({ walletId }) => {
       if (groupedData[monthYear]) {
         groupedData[monthYear].despesas += despesa.valor
       }
-    });
+    })
 
     // Formatando os dados para o formato esperado pelo Recharts
     return Object.keys(groupedData).sort().map((key) => {
       return { month: key, ...groupedData[key] }
-    });
-  };
-
+    })
+  }
 
   // Função para formatar o nome do mês
   const formatMonthName = (monthString: string) => {
     const [year, month] = monthString.split('-')
     const date = new Date(parseInt(year), parseInt(month) - 1)
     return date.toLocaleString('default', { month: 'long' })
-  };
+  }
 
   return (
     <ResponsiveContainer width="95%" height={350}>
