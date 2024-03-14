e8a3057daab1cd3ce0739feacf6892f4c1c1b389
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-03-14 10:44:36 -0300

    Update Grafico.tsx

--- frontend/src/components/Graficos/Grafico.tsx
+++ frontend/src/components/Graficos/Grafico.tsx
@@ -15,69 +15,26 @@ const Grafico: React.FC = () => {
   const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
-    Promise.all([
-      fetch('http://localhost:3333/ativosWallet/1')
-        .then(response => response.json())
-        .then(ativos => {
-          console.log('Dados de ativos:', ativos);
-          return ativos;
-        }),
-      fetch('http://localhost:3333/despesasWallet/1')
-        .then(response => response.json())
-        .then(despesas => {
-          console.log('Dados de despesas:', despesas);
-          return despesas;
-        })
-    ])
-    .then(([ativos, despesas]) => {
-      const dataByMonthAtivos = groupDataByMonthAtivos(ativos);
-      const dataByMonthDespesa = groupDataByMonthDespesa(despesas);
-      setData([...dataByMonthAtivos, ...dataByMonthDespesa]);
-    })
-    .catch(error => console.error('Erro ao buscar dados:', error));
-  }, []);
+    const fetchData = async () => {
+      try {
+        const responseDespesas = await fetch('http://localhost:3333/despesasWallet/1');
+        const despesas: Despesa[] = await responseDespesas.json();
+        console.log('Dados de despesas:', despesas);
+        
+        const responseAtivos = await fetch('http://localhost:3333/ativosWallet/1');
+        const ativos: Ativo[] = await responseAtivos.json();
+        console.log('Dados de ativos:', ativos);
 
-  const groupDataByMonthAtivos = (data: Ativo[]) => {
-    const groupedData: any = {};
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth();
-    const currentYear = currentDate.getFullYear();
-    
-    // Adiciona os últimos 3 meses
-    for (let i = currentMonth - 2; i <= currentMonth; i++) {
-      const month = (i < 0 ? 12 + i : i) + 1; 
-      const year = i < 0 ? currentYear - 1 : currentYear;
-      groupedData[`${year}-${month}`] = [];
-    }
-    
-    // Adiciona os próximos 6 meses
-    for (let i = 1; i <= 6; i++) {
-      const month = currentMonth + i;
-      const year = currentYear + Math.floor((currentMonth + i) / 12);
-      groupedData[`${year}-${(month % 12) || 12}`] = [];
-    }
-    
-    // Preenche os dados existentes
-    data.forEach((item: any) => {
-      const date = new Date(item.data); // Supondo que 'data' seja a propriedade que contém a data
-      const month = date.getMonth();
-      const year = date.getFullYear();
-      const key = `${year}-${month + 1}`; // +1 pois os meses vão de 0 a 11
-
-      if (!groupedData[key]) {
-        groupedData[key] = [];
+        const dataByMonthDespesa = groupDataByMonthDespesa(despesas);
+        const dataByMonthAtivos = groupDataByMonthAtivos(ativos);
+        setData([...dataByMonthAtivos, ...dataByMonthDespesa]);
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
       }
+    };
 
-      groupedData[key].push(item);
-    });
-
-    // Formatando os dados para o formato esperado pelo Recharts
-    return Object.keys(groupedData).map((key) => {
-      const monthData = groupedData[key];
-      const totalValue = monthData.reduce((acc: number, curr: any) => acc + curr.valor, 0);
-      return { month: key, totalValue };
-    });
-  };
+    fetchData();
+  }, []);
 
   const groupDataByMonthDespesa = (data: Despesa[]) => {
     console.log("Dados de despesas recebidos:", data)
@@ -121,10 +78,51 @@ const Grafico: React.FC = () => {
       const totalValueDespesa = monthData.reduce((acc: number, curr: any) => acc + curr.valorDespesa, 0);
       
       return { month: key, totalValueDespesa };
-      console.log("totalValueDespesa", totalValueDespesa)
+      
     });
-  };
-  
+  }
+
+  const groupDataByMonthAtivos = (data: Ativo[]) => {
+    const groupedData: any = {};
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+    
+    // Adiciona os últimos 3 meses
+    for (let i = currentMonth - 2; i <= currentMonth; i++) {
+      const month = (i < 0 ? 12 + i : i) + 1; 
+      const year = i < 0 ? currentYear - 1 : currentYear;
+      groupedData[`${year}-${month}`] = [];
+    }
+    
+    // Adiciona os próximos 6 meses
+    for (let i = 1; i <= 6; i++) {
+      const month = currentMonth + i;
+      const year = currentYear + Math.floor((currentMonth + i) / 12);
+      groupedData[`${year}-${(month % 12) || 12}`] = [];
+    }
+    
+    // Preenche os dados existentes
+    data.forEach((item: any) => {
+      const date = new Date(item.data); // Supondo que 'data' seja a propriedade que contém a data
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const key = `${year}-${month + 1}`; // +1 pois os meses vão de 0 a 11
+
+      if (!groupedData[key]) {
+        groupedData[key] = [];
+      }
+
+      groupedData[key].push(item);
+    });
+
+    // Formatando os dados para o formato esperado pelo Recharts
+    return Object.keys(groupedData).map((key) => {
+      const monthData = groupedData[key];
+      const totalValueAtivos = monthData.reduce((acc: number, curr: any) => acc + curr.valor, 0);
+      return { month: key, totalValueAtivos };
+    });
+  }
 
   return (
     <ResponsiveContainer width="95%" height={350}>
@@ -134,8 +132,8 @@ const Grafico: React.FC = () => {
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="totalValue" name="Ativos" fill="#b0ff00" />
-        <Bar dataKey="totalValueDespesa" name="Despesas" fill="#ff0000" />
+        <Bar dataKey="totalValueAtivos" name="Ativos" fill="#b0ff00" />
+        <Bar dataKey="totalValueDespesas" name="Despesas" fill="#ff0000" />
       </BarChart>
     </ResponsiveContainer>
   );
