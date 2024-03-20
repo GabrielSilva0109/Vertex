852e75c64779b9bd8b87027dfbfeb9432256c8c2
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-03-20 12:36:13 -0300

    Update Grafico.tsx

--- frontend/src/components/Graficos/Grafico.tsx
+++ frontend/src/components/Graficos/Grafico.tsx
@@ -13,6 +13,12 @@ interface Despesa {
   valor: number
 }
 
+interface Investimento {
+  data: string | number | Date
+  titulo: string
+  valor: number
+}
+
 interface GraficoProps {
   walletId: number
 }
@@ -21,10 +27,12 @@ const CustomTooltip = ({ active, payload, label }: any) => {
   if (active && payload && payload.length) {
     const ativos = payload.find((entry: any) => entry.dataKey === 'ativos');
     const despesas = payload.find((entry: any) => entry.dataKey === 'despesas');
+    const investimentos = payload.find((entry: any) => entry.dataKey === 'investimentos')
     return (
       <div className="custom-tooltip">
         {ativos && <p>{`Ativos: R$${ativos.value}`}</p>}
         {despesas && <p>{`Despesas: R$${despesas.value}`}</p>}
+        {despesas && <p>{`Investimentos: R$${investimentos.value}`}</p>}
       </div>
     );
   }
@@ -38,11 +46,13 @@ const Grafico: React.FC<GraficoProps> = ({ walletId }) => {
     try {
       const responseAtivos = await fetch(`http://localhost:3333/ativosWallet/${walletId}`)
       const responseDespesas = await fetch(`http://localhost:3333/despesasWallet/${walletId}`)
-      
+      const responseInvestimentos = await fetch(`http://localhost:3333/walletInvestimentos/${walletId}`)
+
       const ativos: Ativo[] = await responseAtivos.json()
       const despesas: Despesa[] = await responseDespesas.json()
+      const investimentos: Investimento[] = await responseInvestimentos.json()
 
-      const dataByMonth = groupDataByMonth(ativos, despesas)
+      const dataByMonth = groupDataByMonth(ativos, despesas, investimentos)
       setData([...dataByMonth])
     } catch (error) {
       console.error('Erro ao buscar dados:', error)
@@ -53,7 +63,7 @@ const Grafico: React.FC<GraficoProps> = ({ walletId }) => {
     fetchData()
   }, [walletId])
 
-  const groupDataByMonth = (ativos: Ativo[], despesas: Despesa[]) => {
+  const groupDataByMonth = (ativos: Ativo[], despesas: Despesa[], investimentos: Investimento[]) => {
     const groupedData: any = {}
     const currentDate = new Date()
     const currentMonth = currentDate.getMonth()
@@ -61,17 +71,18 @@ const Grafico: React.FC<GraficoProps> = ({ walletId }) => {
 
     // Adiciona os últimos 3 meses
     for (let i = currentMonth - 2; i <= currentMonth; i++) {
-      const month = (i < 0 ? 12 + i : i) + 1
-      const year = i < 0 ? currentYear - 1 : currentYear
-      groupedData[`${year}-${month}`] = { ativos: 0, despesas: 0 }
+      const month = (i < 0 ? 12 + i : i) + 1;
+      const year = i < 0 ? currentYear - 1 : currentYear;
+      groupedData[`${year}-${month}`] = { ativos: 0, despesas: 0, investimentos: 0 }
     }
 
     // Adiciona os próximos 6 meses
     for (let i = 1; i <= 6; i++) {
-      const month = currentMonth + i
-      const year = currentYear + Math.floor((currentMonth + i) / 12)
-      groupedData[`${year}-${(month % 12) || 12}`] = { ativos: 0, despesas: 0 }
+      const month = currentMonth + i;
+      const year = currentYear + Math.floor((currentMonth + i) / 12);
+      groupedData[`${year}-${(month % 12) || 12}`] = { ativos: 0, despesas: 0, investimentos: 0 }
     }
+
 
     // Preenche os dados existentes
     ativos.forEach((ativo: Ativo) => {
@@ -87,6 +98,14 @@ const Grafico: React.FC<GraficoProps> = ({ walletId }) => {
       const monthYear = `${date.getFullYear()}-${date.getMonth() + 1}`
       if (groupedData[monthYear]) {
         groupedData[monthYear].despesas += despesa.valor
+      }
+    })
+
+    investimentos.forEach((investimento: Investimento) => {
+      const date = new Date(investimento.data)
+      const monthYear = `${date.getFullYear()}-${date.getMonth() + 1}`
+      if (groupedData[monthYear]) {
+        groupedData[monthYear].investimentos += investimento.valor
       }
     })
 
@@ -113,6 +132,7 @@ const Grafico: React.FC<GraficoProps> = ({ walletId }) => {
         <Legend />
         <Bar dataKey="ativos" name="Ativos" fill="#b0ff00" />
         <Bar dataKey="despesas" name="Despesas" fill="#ff0000" />
+        <Bar dataKey="investimentos" name="Investimentos" fill="#ffffff" />
       </BarChart>
     </ResponsiveContainer>
   )
