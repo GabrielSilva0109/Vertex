18b26045a2ff7b2182f3953c2d1f22b9053c1c85
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-03-21 11:24:51 -0300

    Update InvestimentoContent.tsx

--- frontend/src/components/Investimento/InvestimentoContent.tsx
+++ frontend/src/components/Investimento/InvestimentoContent.tsx
@@ -269,6 +269,16 @@ const InvestimentoContent: React.FC = () => {
   const [investimentosFiis, setInvestimentosFiis] = useState<any[]>([])
   const [investimentosRendaFixa, setInvestimentosRendaFixa] = useState<any[]>([])
   const [investimentosPoupanca, setInvestimentosPoupanca] = useState<any[]>([])
+
+  const [investimentoSelecionado, setInvestimentoSelecionado] = useState<any>(null)
+  const openEditModal = (investimento: any) => {
+    setInvestimentoSelecionado(investimento);
+    setModalOpen(true);
+  }
+  const closeEditModal = () => {
+    setInvestimentoSelecionado(null);
+    setModalOpen(false);
+  }
   
   const colors = ['#b0ff00', '#005954', '#338b85', '#ffcc00', '#9ce0db', '#4f46e5']
   const [expandedBoxes, setExpandedBoxes] = useState<{ [key: string]: boolean }>({
@@ -379,25 +389,29 @@ const InvestimentoContent: React.FC = () => {
   const editInvestimento = async (investimentoId: number, novosDados: any) => {
     try {
       const response = await fetch(`http://localhost:3333/investimento/${investimentoId}`, {
-        method: "PUT",
+        method: 'PUT',
         headers: {
-          "Conten-Type": "application/json"
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(novosDados)
-      })
-
-      if(!response.ok) {
+      });
+  
+      if (!response.ok) {
         throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
       }
-
-      toast.success('Investimento atualizado com sucesso!')
-
-      await fetchInvestimentos(wallet_id)
-
-    } catch (error){
-      console.log('Erro ao editar o Investimento', error)
+  
+      toast.success('Investimento atualizado com sucesso!');
+  
+      // Feche o modal após a atualização do investimento
+      closeEditModal();
+  
+      // Atualize a lista de investimentos após a atualização bem-sucedida
+      await fetchInvestimentos(wallet_id);
+    } catch (error) {
+      console.error('Erro ao editar o investimento:', error);
     }
   }
+  
 
   useEffect(() => {
     if (user && user.id) {
@@ -434,7 +448,7 @@ const InvestimentoContent: React.FC = () => {
                     <p>Valor: R${investimento.valor}</p>
 
                     <Btns>
-                    <BtnEdit >
+                    <BtnEdit onClick={() => editInvestimento(investimento.id, novosDados)} >
                       <svg height="1em" viewBox="0 0 512 512">
                         <path
                           d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
@@ -611,14 +625,16 @@ const InvestimentoContent: React.FC = () => {
           <BtnAtivo onClick={openModal}>Adicionar Ativo</BtnAtivo>
             {modalOpen && (
               <ModalInvestimento
-              onClose={closeModal}
-              walletId={wallet_id}
-              onSubmit={(requestData: any) => {
-                setInvestimentoAdicionado(true);
-              } } fetchInvestimentos={function (): void {
-                throw new Error('Function not implemented.');
-              } }            />
-            )}
+                onClose={closeModal}
+                walletId={wallet_id}
+                investimento={investimentoSelecionado}
+                onSubmit={(requestData: any) => {
+                  editInvestimento(investimentoSelecionado.id, requestData)
+                  setInvestimentoAdicionado(true);
+                } } fetchInvestimentos={function (): void {
+                  throw new Error('Function not implemented.');
+                } }/>
+              )}
         </BoxRight>
       </RightContainer>
     </Container>
