367c9f6d4a7e7f20c62b294527c37e8edbc3f5be
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-03-26 11:41:53 -0300

    1

--- frontend/src/components/Investimento/InvestimentoContent.tsx
+++ frontend/src/components/Investimento/InvestimentoContent.tsx
@@ -4,6 +4,7 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import CircleGrafico from '../Graficos/CircleGrafico'
 import ModalInvestimento from './ModalInvestimento'
+import ModalEdit from './ModalEdit'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { error } from 'console'
@@ -269,16 +270,7 @@ const InvestimentoContent: React.FC = () => {
   const [investimentosFiis, setInvestimentosFiis] = useState<any[]>([])
   const [investimentosRendaFixa, setInvestimentosRendaFixa] = useState<any[]>([])
   const [investimentosPoupanca, setInvestimentosPoupanca] = useState<any[]>([])
-
   const [investimentoSelecionado, setInvestimentoSelecionado] = useState<any>(null)
-  const openEditModal = (investimento: any) => {
-    setInvestimentoSelecionado(investimento);
-    setModalOpen(true);
-  }
-  const closeEditModal = () => {
-    setInvestimentoSelecionado(null);
-    setModalOpen(false);
-  }
   
   const colors = ['#b0ff00', '#005954', '#338b85', '#ffcc00', '#9ce0db', '#4f46e5']
   const [expandedBoxes, setExpandedBoxes] = useState<{ [key: string]: boolean }>({
@@ -290,6 +282,7 @@ const InvestimentoContent: React.FC = () => {
     poupanca: false,
   })
   const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpenEdit, setModalOpenEdit] = useState(false)
 
   const openModal = () => {
     setModalOpen(true)
@@ -297,6 +290,14 @@ const InvestimentoContent: React.FC = () => {
 
   const closeModal = () => {
     setModalOpen(false);
+  }
+
+  const openModalEdit = () => {
+    setModalOpenEdit(true)
+  }
+
+  const closeModalEdit = () => {
+    setModalOpenEdit(false);
   }
 
   const toggleExpandedBox = (boxKey: string) => {
@@ -404,7 +405,7 @@ const InvestimentoContent: React.FC = () => {
       toast.success('Investimento atualizado com sucesso!')
   
       
-      closeEditModal();
+      closeModalEdit()
   
       // Atualize a lista de investimentos após a atualização bem-sucedida
       await fetchInvestimentos(wallet_id);
@@ -449,7 +450,7 @@ const InvestimentoContent: React.FC = () => {
                     <p>Valor: R${investimento.valor}</p>
 
                     <Btns>
-                    <BtnEdit onClick={openModal} >
+                    <BtnEdit onClick={openModalEdit} >
                       <svg height="1em" viewBox="0 0 512 512">
                         <path
                           d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
--- frontend/src/components/Investimento/ModalEdit.tsx
+++ frontend/src/components/Investimento/ModalEdit.tsx
@@ -1,11 +1,122 @@
-import React from "react"
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Form } from '../Carteira/ModalCarteira';
+import { BtnAtivo, BtnDespesa } from '../Carteira/CarteiraContent';
+import { Input } from '../Login/FormLogin';
+import { toast } from 'react-toastify';
 
+interface ModalProps {
+    onClose: () => void
+    walletId: number
+    investimento?: any
+    onSubmit: (requestData: any) => void
+    fetchInvestimentos: () => void
+    children?: React.ReactNode
+}
 
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
-const ModalEdit: React.FC = () => {
-    return (
-        <div>s</div>
-    )
+const ModalContent = styled.div`
+  background-color: gray;
+  padding: 20px;
+  border-radius: 10px;
+`
+
+const Select = styled.select`
+    background-color: #e1e1e1;
+    color: black;
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    padding: 12px 16px;
+    border-radius: 8px;
+    border: 1px solid #b0ff00;
+    outline: none;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        background-color: #f1f1f1;
+        border-color: #667788;
+    }
+
+    &:focus {
+        background-color: #272727;
+        border-color: #b0ff00;
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+    }
+
+    &.invalid {
+        border-color: #ff0000;
+        background-color: #f00;
+    }
+
+    /* Animações */
+    &.entering {
+        animation: slide-in 0.3s ease-in-out forwards;
+    }
+`
+
+const Option = styled.option`
+  background-color: #f1f1f1;
+  color: black;
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+`
+
+const ModalEdit: React.FC<ModalProps> = ({ onClose, walletId, onSubmit, fetchInvestimentos }) => {
+  const [titulo, setTitulo] = useState('')
+  const [valor, setValor] = useState('')
+  const [quantidade, setQuantidade] = useState('')
+  const [categoria, setCategoria] = useState('')
+  const [data, setData] = useState('')
+
+  const handleSubmit = () => {
+    const requestData = {
+      walletId: walletId,
+      titulo: titulo,
+      valor: valor,
+      quantidade: quantidade,
+      categoria: categoria,
+      data: data
+    }
+    onSubmit(requestData)
+  }
+  
+  return (
+    <ModalOverlay>
+      <ModalContent>
+        <Form>
+          <h1>Atualizar</h1>
+          <Input type='text' placeholder='Ativo' value={titulo} onChange={(e) => setTitulo(e.target.value)} />
+          <Input type='number' placeholder='Valor' value={valor} onChange={(e) => setValor(e.target.value)} />
+          <Input type='number' placeholder='Quantidade' value={quantidade} onChange={(e) => setQuantidade(e.target.value)} />
+          <Select id="categoria" value={categoria} onChange={(e) => setCategoria(e.target.value)}>
+            <option value="">Selecione uma categoria</option>
+            <option value="Ação">Ações</option>
+            <option value="Crypto">Crypto</option>
+            <option value="Moeda">Moeda</option>
+            <option value="FIIs">FIIs</option>
+            <option value="Renda Fixa">Renda Fixa</option>
+            <option value="Poupança">Poupança</option>
+          </Select>
+          <Input type='date' placeholder='Data' value={data} onChange={(e) => setData(e.target.value)} />
+          
+          <BtnDespesa onClick={onClose}>Fechar</BtnDespesa>
+        </Form>
+      </ModalContent>
+    </ModalOverlay>
+  )
 }
 
 export default ModalEdit
