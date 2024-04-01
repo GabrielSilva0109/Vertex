76df905cfb7aa442b39e38222760b0b134d2cc25
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-04-01 13:14:04 -0300

    Update NoticiasContent.tsx

--- frontend/src/components/Noticias/NoticiasContent.tsx
+++ frontend/src/components/Noticias/NoticiasContent.tsx
@@ -4,15 +4,12 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: start;
   background: rgb(28, 28, 30);
   width: 100%;
   min-height: 100vh;
   color: white;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
 `
 
 const BoxCrypto = styled.div`
@@ -23,28 +20,111 @@ const BoxCrypto = styled.div`
   border-radius: 20px;
   width: 70%;
   background: #2C2C2C;
-  height: 300px;
+  height: 400px;
+  margin-bottom: 10px;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+    width: 100%;
+  }
+`
+
+const BoxAcoes = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 30px;
+  border-radius: 20px;
+  width: 70%;
+  background: #b0ff00;
+  height: 400px;
+  margin-left: 370px;
+
+  @media (max-width: 768px) {
+    margin-left: 0;
+    width: 100%;
+  }
+`
+
+const Image = styled.img`
+  width: 100%;
+  max-height: 40%;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  background: linear-gradient(#e66465, #9198e5);
+  transition: height 0.3s ease;
 `
 
 const Item = styled.div`
+  width: 30%;
+  height: 350px;
+  background:black;
+  border-radius: 30px;
+  margin: 10px;
   display: flex;
   flex-direction: column;
-  gap: 10px;
-  margin: 10px;
-  align-items: center; 
+  align-items: center;
+  transition: transform 0.3s ease;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
 `
 
-const Text = styled.p`
+const Title = styled.h4`
+  align-self: flex-start;
+  padding: 0px 10px;
   font-size: 0.8rem;
+`
+
+const Description = styled.p`
+  font-weight: bold;
+  padding: 0px 10px;
+  font-size: 0.8rem;
+  color: #999999;
+  max-height: 100px;
+  overflow-y: auto; 
+  margin-bottom: 10px; 
+
+  /* Estilos para o scrollbar */
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  /* Track */
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  /* Handle */
+  &::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 4px;
+  }
+
+  /* Handle on hover */
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`
+
+const CustomLink = styled.a`
+  color: black;
+  width: 60px;
+  font-size: 0.8rem;
+  font-weight: bold;
+  text-decoration: none;
+  background: #b0ff00;
+  padding: 8px;
+  border-radius: 10px;
+  margin-top: auto;
+  margin-bottom: 10px;
 `
 
 const Button = styled.button`
   background-color: #1c1c1e ;
   border-radius: 50px;
   cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   margin: 10px;
   color: white;
   font-size: 1.2em;
@@ -56,56 +136,96 @@ const Button = styled.button`
   &:hover {
     background-color: white;
   }
-
-
 `
 
 const NoticiasContent: React.FC = () => {
-  const { state } = useLocation()
-  const [noticias, setNoticias] = useState([])
-  const [startIndex, setStartIndex] = useState(0);
+  const [noticiasGerais, setNoticiasGerais] = useState([])
+  const [noticiasCrypto, setNoticiasCrypto] = useState([])
+  const [startIndexGerais, setStartIndexGerais] = useState(0)
+  const [startIndexCrypto, setStartIndexCrypto] = useState(0)
 
   useEffect(() => {
-    async function fetchNoticias() {
+    async function fetchNoticiasGerais() {
       try {
-        const response = await fetch("https://newsapi.org/v2/everything?q=bitcoin&language=pt&apiKey=3ba975a9509b47f9958e5534f814dec7")
+        const response = await fetch("https://newsapi.org/v2/everything?q=apple&language=pt&apiKey=3ba975a9509b47f9958e5534f814dec7")
         const data = await response.json()
-        setNoticias(data.articles)
+        setNoticiasGerais(data.articles)
       } catch (error) {
-        console.error('Erro ao buscar notícias:', error)
+        console.error('Erro ao buscar notícias gerais:', error)
       }
     }
-    fetchNoticias()
+
+    async function fetchNoticiasCrypto() {
+      try {
+        const response = await fetch("https://newsapi.org/v2/everything?q=Crypto&language=pt&apiKey=3ba975a9509b47f9958e5534f814dec7")
+        const data = await response.json()
+        setNoticiasCrypto(data.articles)
+      } catch (error) {
+        console.error('Erro ao buscar notícias sobre criptomoedas:', error)
+      }
+    }
+
+    fetchNoticiasGerais()
+    fetchNoticiasCrypto()
   }, [])
 
-  const handleNext = () => {
-    setStartIndex(startIndex + 3);
+  const handleNextGerais = () => {
+    setStartIndexGerais(startIndexGerais + 3)
   }
 
-  const handlePrevious = () => {
-    setStartIndex(Math.max(0, startIndex - 3));
+  const handlePreviousGerais = () => {
+    setStartIndexGerais(Math.max(0, startIndexGerais - 3))
+  }
+
+  const handleNextCrypto = () => {
+    setStartIndexCrypto(startIndexCrypto + 3)
+  }
+
+  const handlePreviousCrypto = () => {
+    setStartIndexCrypto(Math.max(0, startIndexCrypto - 3))
   }
 
   return (
     <Container>
       <BoxCrypto>
         <div>
-        <Button onClick={handlePrevious} disabled={startIndex === 0}>-</Button>
+          <Button onClick={handlePreviousGerais} disabled={startIndexGerais === 0}>-</Button>
         </div>
         
-          {noticias.slice(startIndex, startIndex + 3).map((noticia: any, index: number) => (
-            <Item key={index}>
-              <h4>{noticia.title}</h4>
-              <Text>{noticia.description}</Text>
-              <a href={noticia.url} target="_blank" rel="noopener noreferrer">Leia mais</a>
-            </Item>
-          ))}
+        {noticiasGerais.slice(startIndexGerais, startIndexGerais + 3).map((noticia: any, index: number) => (
+          <Item key={index}>
+            <Image src={noticia.urlToImage} alt="Imagem da notícia" />
+            <Title>{noticia.title}</Title>
+            <Description>{noticia.description}</Description>
+            <CustomLink href={noticia.url} target="_blank" rel="noopener noreferrer">Leia mais</CustomLink>
+          </Item>
+        ))}
+        
         <div>
-          <Button onClick={handleNext} disabled={startIndex + 3 >= noticias.length}>+</Button>
+          <Button onClick={handleNextGerais} disabled={startIndexGerais + 3 >= noticiasGerais.length}>+</Button>
         </div>
       </BoxCrypto>
+
+      <BoxAcoes>
+        <div>
+          <Button onClick={handlePreviousCrypto} disabled={startIndexCrypto === 0}>-</Button>
+        </div>
+        
+        {noticiasCrypto.slice(startIndexCrypto, startIndexCrypto + 3).map((noticia: any, index: number) => (
+          <Item key={index}>
+            <Image src={noticia.urlToImage} alt="Imagem da notícia" />
+            <Title>{noticia.title}</Title>
+            <Description>{noticia.description}</Description>
+            <CustomLink href={noticia.url} target="_blank" rel="noopener noreferrer">Leia mais</CustomLink>
+          </Item>
+        ))}
+        
+        <div>
+          <Button onClick={handleNextCrypto} disabled={startIndexCrypto + 3 >= noticiasCrypto.length}>+</Button>
+        </div>
+      </BoxAcoes>
     </Container>
   ) 
 }
 
-export default NoticiasContent;
+export default NoticiasContent
