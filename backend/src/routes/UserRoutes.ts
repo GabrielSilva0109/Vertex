e5157cf86fa970c0d3e80c89c0e83dad6914f2d4
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-04-08 10:34:50 -0300

    Add Picture

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,13 +1,26 @@
-import express from 'express'
-import { createUser, deleteUser, getUserById, getUsers, updateUser, loginUser } from '../controllers/UserController'
+import express, { Router } from 'express'
+import multer from 'multer' 
+import { createUser, deleteUser, getUserById, getUsers, updateUser, loginUser, updateUserPicture } from '../controllers/UserController'
 
-const router = express.Router()
+const router: Router = express.Router()
 
-router.get('/users', getUsers)
-router.get('/users/:id', getUserById)
-router.post('/user', createUser)
-router.put('/user/:id', updateUser)
-router.delete('/user/:id', deleteUser)
-router.post('/login', loginUser)
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+});
 
-export default router
+const upload = multer({ storage: storage });
+
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+router.post('/user', createUser);
+router.put('/userPicture/:id', upload.single('picture'), updateUserPicture)
+router.put('/user/:id', updateUser);
+router.delete('/user/:id', deleteUser);
+router.post('/login', loginUser);
+
+export default router;
