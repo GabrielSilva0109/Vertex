df38ced3d029cdaaab3067c34837ff56695d6d6a
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-04-22 10:42:25 -0300

    Update EmailController.ts

--- backend/src/controllers/EmailController.ts
+++ backend/src/controllers/EmailController.ts
@@ -1,23 +1,25 @@
-// emailService.ts
 import { mailgunConfig } from '../mailGunConfig'
 import mailgun from 'mailgun-js'
 import { Request, Response } from "express"
 
-const mg = mailgun({apiKey: mailgunConfig.apiKey, domain: mailgunConfig.domain});
+const mg = mailgun({apiKey: mailgunConfig.apiKey, domain: mailgunConfig.domain
 
-export const enviarEmail = (destinatario: string, assunto: string, corpo: string) => {
+export const sendEmailWelcome = (recipient: string) => {
+    const subject = "Bem-vindo ao Vertex"
+    const body = "Olá e bem-vindo ao Vertex! Estamos entusiasmados por tê-lo(a) conosco. Sua experiência é nossa prioridade, e estamos aqui para garantir que seja excepcional. Aproveite ao máximo cada momento aqui!"
+
     const data = {
-        from: 'silvagabriel0109@gmail.com',
-        to: destinatario,
-        subject: assunto,
-        text: corpo
-    };
+        from: 'automacaobrame@gmail.com',
+        to: recipient,
+        subject: subject,
+        text: body
+    }
 
     mg.messages().send(data, (error: any, body: any) => {
-        if (error) {
-            console.error('Erro ao enviar email:', error);
+        if(error){
+            console.error("Erro ao enviar o email!", error)
         } else {
-            console.log('Email enviado com sucesso:', body);
+            console.log('Email de Boas vindas enviado !', body)
         }
-    });
-};
+    })
+}
