a95aeb809fca19a15132a512cfb768a370cb4286
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-04-22 10:31:08 -0300

    Update mailGunConfig.ts

---
+++ backend/src/mailGunConfig.ts
@@ -0,0 +1,5 @@
+// mailgunConfig.ts
+export const mailgunConfig = {
+    apiKey: 'a3023ed7f34091716ac9baa63656e718-2175ccc2-05f6f4ca',
+    domain: 'sandboxf75d53782fdf4f4faf81cd50f0bfa3fb.mailgun.org'
+};
