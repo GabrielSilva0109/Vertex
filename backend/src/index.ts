a6f6739ab3bba624a7141140d6a74042a20dd330
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-04-23 10:36:58 -0300

    1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,73 +7,16 @@ import walletRoutes from './routes/WalletRoutes'
 import ativoRoutes from './routes/AtivoRoutes'
 import despesaRoutes from './routes/DespesaRoutes'
 import investimentoRoutes from './routes/InvestimentoRoutes'
-const { google } = require('googleapis')
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 const port = 3333;
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = 'http://localhost:3333/oauth2callback'
+const REDIRECT_URI = 'http://localhost:3333/oauth2callback';
 
-const oAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI
-)
-
-app.use(express.json())
-app.use(cors())
-
-app.get('/oauth2callback', async (req, res) => {
-  const code = req.query.code as string; //Código de autorização da consulta da URL
-
-  try {
-      if (!code) {
-          throw new Error('Código de autorização ausente.');
-      }
-
-      // Trocar o código de autorização pelo refresh token
-      const { tokens } = await oAuth2Client.getToken(code);
-      const refreshToken = tokens.refresh_token;
-
-      // Agora você tem o refresh token, você pode usar isso para enviar e-mails
-      // Configurações do transporte SMTP
-      const transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-              type: 'OAuth2',
-              user: 'vertexcash@gmail.com',
-              clientId: CLIENT_ID,
-              clientSecret: CLIENT_SECRET,
-              refreshToken: refreshToken,
-          },
-      });
-
-      // Obter o destinatário do corpo da solicitação ou de qualquer outra fonte de dados
-      let destinatario = 'destinatario@example.com'; // Destinatário padrão
-      if (typeof req.query.destinatario === 'string') {
-          destinatario = req.query.destinatario;
-      }
-
-      // Configurações do e-mail a ser enviado
-      const mailOptions = {
-          from: 'vertexcash@gmail.com',
-          to: destinatario,
-          subject: 'Assunto do e-mail',
-          text: 'Corpo do e-mail',
-      };
-
-      const result = await transporter.sendMail(mailOptions);
-      console.log('E-mail enviado:', result);
-      res.send('E-mail enviado com sucesso!');
-  } catch (error) {
-      console.error('Erro ao enviar e-mail:', error);
-      res.status(500).send('Erro ao enviar e-mail.');
-  }
-})
 
 // Rotas principais
 app.get('/', (req, res) => {
