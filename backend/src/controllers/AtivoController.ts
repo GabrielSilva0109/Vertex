397679f7a783f81310110dd4ffb9687699e52a41
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-04-30 09:21:30 -0300

    Update AtivoController.ts

--- backend/src/controllers/AtivoController.ts
+++ backend/src/controllers/AtivoController.ts
@@ -1,30 +1,30 @@
-import { localDB } from "../db"
+import { localDB, awsDB } from "../db"
 import { Request, Response } from 'express'
 
 // Retorna todos os ATIVOS
 export const getAtivos = async (req: Request, res: Response) => {
-    const q = "SELECT * FROM ativos;";  
-    localDB.query(q, (erro, data) => {
-        if (erro) return res.status(500).json({ erro: "Erro ao trazer os Ativos" });
-        return res.status(200).json(data);
-    });
+    const q = "SELECT * FROM ativos;"
+    awsDB.query(q, (erro, data) => {
+        if (erro) return res.status(500).json({ erro: "Erro ao trazer os Ativos" })
+        return res.status(200).json(data)
+    })
 }
 
 // Retorna ATIVO por ID
 export const getAtivoById = async (req: Request, res: Response) => {
-    const q = "SELECT * FROM ativos WHERE `id`=?;"; 
+    const q = "SELECT * FROM ativos WHERE `id`=?;" 
 
-    localDB.query(q, [req.params.id], (erro, data) => {
-        if (erro) return res.status(500).json({ erro: "Erro ao trazer Ativo por ID" });
-        return res.status(200).json(data[0]);
-    });
+    awsDB.query(q, [req.params.id], (erro, data) => {
+        if (erro) return res.status(500).json({ erro: "Erro ao trazer Ativo por ID" })
+        return res.status(200).json(data[0])
+    })
 }
 
 // Retorna ATIVOS por ID da WALLET
 export const getAtivosByWalletId = async (req: Request, res: Response) => {
     const q = "SELECT * FROM Ativos WHERE `wallet_id`=?"
 
-    localDB.query(q, [req.params.id], (erro, data) => {
+    awsDB.query(q, [req.params.id], (erro, data) => {
         if(erro) return res.status(500).json({erro: "Erro ao trazer os Ativos dessa Wallet"})
         return res.status(200).json(data)
     })
@@ -32,32 +32,32 @@ export const getAtivosByWalletId = async (req: Request, res: Response) => {
 
 // Cria o ATIVO
 export const createAtivo = async (req: Request, res: Response) => {
-    const { wallet_id, titulo, valor, observacao, categoria, fonte, data } = req.body; 
+    const { wallet_id, titulo, valor, observacao, categoria, fonte, data } = req.body
 
     if (!titulo || !valor || !wallet_id) {
-        return res.status(400).json({ erro: "Campos Obrigatórios!" });
+        return res.status(400).json({ erro: "Campos Obrigatórios!" })
     }
 
-    const q = "INSERT INTO ativos(`wallet_id`, `titulo`, `valor`, `observacao`, `categoria`, `fonte`, `data`) VALUES (?,?,?,?,?,?,?);";  // Alteração do nome da tabela para 'ativos'
-    localDB.query(q, [wallet_id, titulo, valor, observacao, categoria, fonte, data], (erro, data) => {
-        if (erro) return res.status(500).json({ erro: "Erro ao Cadastrar o Ativo" });
-        return res.status(201).json("Cadastrado Ativo!");
-    });
+    const q = "INSERT INTO ativos(`wallet_id`, `titulo`, `valor`, `observacao`, `categoria`, `fonte`, `data`) VALUES (?,?,?,?,?,?,?);" // Alteração do nome da tabela para 'ativos'
+    awsDB.query(q, [wallet_id, titulo, valor, observacao, categoria, fonte, data], (erro, data) => {
+        if (erro) return res.status(500).json({ erro: "Erro ao Cadastrar o Ativo" })
+        return res.status(201).json("Cadastrado Ativo!")
+    })
 }
 
 // Atualiza o ATIVO
 export const updateAtivo = async (req: Request, res: Response) => {
-    const ativoId = req.params.id;
-    const { titulo, valor, observacao, categoria, fonte, data } = req.body;  
+    const ativoId = req.params.id
+    const { titulo, valor, observacao, categoria, fonte, data } = req.body
 
     // Construir a parte SET dinamicamente com base nos campos fornecidos pelo usuário
     const setFields = [];
-    if (titulo !== undefined) setFields.push("titulo=?");
-    if (valor !== undefined) setFields.push("valor=?");
-    if (observacao !== undefined) setFields.push("observacao=?");
-    if (categoria !== undefined) setFields.push("categoria=?");
-    if (fonte !== undefined) setFields.push("fonte=?");
-    if (data !== undefined) setFields.push("data=?");
+    if (titulo !== undefined) setFields.push("titulo=?")
+    if (valor !== undefined) setFields.push("valor=?")
+    if (observacao !== undefined) setFields.push("observacao=?")
+    if (categoria !== undefined) setFields.push("categoria=?")
+    if (fonte !== undefined) setFields.push("fonte=?")
+    if (data !== undefined) setFields.push("data=?")
 
     if (setFields.length === 0) {
         return res.status(400).json({ erro: "Nenhum campo fornecido para atualização" })
@@ -65,20 +65,20 @@ export const updateAtivo = async (req: Request, res: Response) => {
 
     const q = `UPDATE ativos SET ${setFields.join(", ")} WHERE id=?;`
 
-    localDB.query(q, [...Object.values(req.body).filter(value => value !== undefined), ativoId], (erro, data) => {
+    awsDB.query(q, [...Object.values(req.body).filter(value => value !== undefined), ativoId], (erro, data) => {
         if (erro) return res.status(500).json({ erro: "Erro ao Atualizar o Ativo" })
         return res.status(200).json("Ativo Atualizado!")
-    });
+    })
 }
 
 // Excluir o ATIVO
 export const deleteAtivo = async (req: Request, res: Response) => {
-    const ativoId = req.params.id;
+    const ativoId = req.params.id
 
-    const q = "DELETE FROM ativos WHERE id=?;";  // Alteração do nome da tabela para 'ativos'
+    const q = "DELETE FROM ativos WHERE id=?;" // Alteração do nome da tabela para 'ativos'
 
-    localDB.query(q, [ativoId], (erro, data) => {
-        if (erro) return res.status(500).json({ erro: "Erro ao Excluir o Ativo" });
-        return res.status(200).json("Ativo Excluído!");
-    });
+    awsDB.query(q, [ativoId], (erro, data) => {
+        if (erro) return res.status(500).json({ erro: "Erro ao Excluir o Ativo" })
+        return res.status(200).json("Ativo Excluído!")
+    })
 }
