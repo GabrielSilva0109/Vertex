163d8a524a145ae2fdb9c16eebf8847e8c2b8a02
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-09 10:33:38 -0300

    Update Footer.tsx

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -4,16 +4,21 @@ import styled from 'styled-components'
 const Container = styled.div`
     height: 100px;
     background: #1c1c1e;
-    
+    width: 100;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
+const Text = styled.p`
+  font-weight: bold;
+
+`
 
 const Footer: React.FC = () => {
   return (
-    <Container>
-        footer
-
-
+    <Container><span>|</span>
+      <Text>© 2024 Vertex, Inc.</Text>
     </Container>
   )
 }
