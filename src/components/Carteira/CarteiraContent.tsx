3cc45f924b09a3fa189d15a1b3e8c8647f7364d3
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-22 08:48:29 -0300

    Update CarteiraContent.tsx

--- src/components/Carteira/CarteiraContent.tsx
+++ src/components/Carteira/CarteiraContent.tsx
@@ -460,8 +460,8 @@ const CarteiraContent: React.FC = () =>{
     
     const getExtrato = async () => {
       try {
-        const ativosResponse = await fetch(`${baseURL}/api/ativosWallet/${IdWallet}`)
-        const despesasResponse = await fetch(`${baseURL}/api/despesasWallet/${IdWallet}`)
+        const ativosResponse = await fetch(`${baseURL}/ativosWallet/${IdWallet}`)
+        const despesasResponse = await fetch(`${baseURL}/despesasWallet/${IdWallet}`)
   
         const ativos =  (await ativosResponse.json()).map((ativo: any) => ({ ...ativo, categoria: 'ativo'}))
         const despesas = (await despesasResponse.json()).map((despesa: any) => ({...despesa, categoria: 'despesa'}))
